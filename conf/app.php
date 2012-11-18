319b3f2825e66d8f8a6121b47bf108f97f6e988e
lidaa <aa_dil@hotmail.fr>
2012-11-18 12:38:50 +0000

    change config

--- conf/app.php
+++ conf/app.php
@@ -3,5 +3,8 @@
 $app_config = array();
 
 $app_config['default_layout'] = 'layout.html.php';
-$app_config['base_url'] = 'http://lidaa.blog';
-$app_config['assets_url'] = 'http://lidaa.blog/web';
+$app_config['base_url'] = 'http://localhost/simple-php-mvc';
+$app_config['assets_url'] = 'http://localhost/simple-php-mvc/web';
+
+$app_config['params']['uploads_dir'] = __DIR__ . '/../web/img/uploads';
+$app_config['params']['name'] = "value";
